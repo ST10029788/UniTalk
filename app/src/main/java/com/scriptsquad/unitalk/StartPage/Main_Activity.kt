faad857d376246ccc8d7f149887bd52c7fcf3648
Kelisha Naidoo <ST10100775@vcconnect.edu.za>
2024-09-30 22:16:03 +0200

    Main_Activity kt Comments

--- app/src/main/java/com/scriptsquad/unitalk/StartPage/Main_Activity.kt
+++ app/src/main/java/com/scriptsquad/unitalk/StartPage/Main_Activity.kt
@@ -15,24 +15,31 @@ import com.scriptsquad.unitalk.Marketplace.fragments.MyAdsFragment
 //method used from YouTube
 //https://youtu.be/DWIGAkYkpg8?si=um9GgnsGWc9G7KAB
 //channel: Android Knowledge
-
+// Class representing the main activity
 class Main_Activity : AppCompatActivity() {
+    // Late-initialized variables for the activity's binding and Firebase authentication
     private lateinit var binding: ActivityMainBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    // Called when the activity is created
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Inflate the activity's layout and bind it to the activity
         binding = ActivityMainBinding.inflate(layoutInflater)
         firebaseAuth = FirebaseAuth.getInstance()
+        // Check if the user is logged in
         if (firebaseAuth.currentUser == null) {
             startLogInOption()
         }
+        // Set the content view to the activity's layout
         setContentView(binding.root)
 
         binding.toolbarBackBtn.setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
 
+        // Show the home fragment by default
         showHomeFragment()
+        // Set up the bottom navigation view item selected listener
         binding.bottomNav.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.menu_home -> {
@@ -40,6 +47,7 @@ class Main_Activity : AppCompatActivity() {
                     true
                 }
 
+                // Chats menu item
                 R.id.menu_chats -> {
                     if (firebaseAuth.currentUser == null) {
                         Utils.toast(this@Main_Activity, "Log_In_Screen Required")
@@ -52,6 +60,7 @@ class Main_Activity : AppCompatActivity() {
 
                 }
 
+                // Account menu item
                 R.id.menu_account -> {
 
                     if (firebaseAuth.currentUser == null) {
@@ -66,6 +75,7 @@ class Main_Activity : AppCompatActivity() {
 
                 }
 
+                // My Ads menu item
                 R.id.menu_myAds -> {
                     if (firebaseAuth.currentUser == null) {
                         Utils.toast(this@Main_Activity, "Log_In_Screen Required")
@@ -79,6 +89,7 @@ class Main_Activity : AppCompatActivity() {
 
                 }
 
+                // Sell menu item
                 R.id.menu_sell -> {
                     if (firebaseAuth.currentUser == null) {
                         Utils.toast(this@Main_Activity, "Log_In_Screen Required")
@@ -92,12 +103,14 @@ class Main_Activity : AppCompatActivity() {
 
                 }
 
+                // Default case
                 else -> {
                     false
                 }
 
             }
         }
+        // Set up the sell FAB click listener
         binding.sellFab.setOnClickListener {
             val intent = Intent(this, Create_Ad_Activity::class.java)
             intent.putExtra("isEditMode", false)
@@ -106,7 +119,7 @@ class Main_Activity : AppCompatActivity() {
 
     }
 
-
+    // Method to show the home fragment
     private fun showHomeFragment() {
         binding.toolbarTitleTv.text = getString(R.string.home)
         val fragment = HomeFragment()
@@ -115,6 +128,7 @@ class Main_Activity : AppCompatActivity() {
         fragmentTransaction.commit()
     }
 
+    // Method to show the chats fragment
     private fun showChatFragment() {
         binding.toolbarTitleTv.text = getString(R.string.chat)
         val fragment = ChatsFragment()
@@ -125,6 +139,7 @@ class Main_Activity : AppCompatActivity() {
     //method used from YouTube
     //https://youtu.be/67Xmgqe-wLU?si=sgBeLP6mUaXJxbj7
     //channel: Coder
+    // Method to show the my ads fragment
     private fun showMyAdsFragment() {
         binding.toolbarTitleTv.text = getString(R.string.my_ads)
         val fragment = MyAdsFragment()
