31d9f954e646fef9a4a22a1ac3411c8f5b73502d
Aariya <ST10029788@vcconnect.edu.za>
2024-09-30 23:43:51 +0200

    comments

--- app/src/main/java/com/scriptsquad/unitalk/Notes/model/ModelComments.kt
+++ app/src/main/java/com/scriptsquad/unitalk/Notes/model/ModelComments.kt
@@ -1,12 +1,12 @@
 package com.scriptsquad.unitalk.Notes.model
-
+//Data class representing a comment on a book.
 class ModelComments {
 
-    var id:String = ""
-    var bookId:String = ""
-    var timestamp:String = ""
-    var comment:String = ""
-    var uid:String = ""
+    var id: String = ""             // Unique identifier for the comment
+    var bookId: String = ""         // Identifier for the associated book
+    var timestamp: String = ""       // Timestamp when the comment was made
+    var comment: String = ""         // Text of the comment
+    var uid: String = ""             // Unique identifier for the user who commented
 
     constructor()
     constructor(id: String, bookId: String, timestamp: String, comment: String, uid: String) {
